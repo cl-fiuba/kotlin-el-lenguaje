a7a95b320df2142666c3fd6c4fa954ecb591d993
cl-fiuba <cleith@fi.uba.ar>
2024-05-10 00:41:25 -0300

    algunos comentarios

--- hola/Hola.kt
+++ hola/Hola.kt
@@ -1,3 +1,27 @@
-fun main() {
+// comentario
+
+/*
+  comentario
+  de varias 
+  líneas
+*/
+
+// fun declara un función
+fun main() { // main es el `entry point`
     println("Hola esto es kotlin 😺")
+
+    // variables
+    var papas: Int = 10
+
+    papas = 15
+
+    // valores, no pueden cambiar
+    val nombre: String = "Pepino" 
+
 }
+
+// tipo de retorno
+fun suma(a: Int, b: Int): Int {
+    return a + b
+}
+
